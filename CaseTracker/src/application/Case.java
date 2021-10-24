11df27936d7f81fd913b12cb443780c7284d57c2
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2021-10-23 21:09:51 -0600

    Case class implemented

---
+++ CaseTracker/src/application/Case.java
@@ -0,0 +1,56 @@
+package application;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class Case {
+	private boolean active;
+	private LocalTime time;
+	private LocalDate date;
+	private long timeWorked;
+	private String title;
+
+	public Case() {
+		active = false;
+		timeWorked = System.currentTimeMillis();
+		time = LocalTime.now();
+		date = LocalDate.now();
+	}
+
+	public Case(String title) {
+		active = false;
+		timeWorked = System.currentTimeMillis();
+		time = LocalTime.now();
+		date = LocalDate.now();
+		this.title = title;
+	}
+
+	public String getTime() {
+		return time.toString();
+	}
+
+	public String getDate() {
+		return date.toString();
+	}
+
+	public String getDateTime() {
+		LocalDateTime myDateObj = LocalDateTime.now();
+		DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("E, MMM dd yyyy HH:mm:ss");
+		String formattedDate = myDateObj.format(myFormatObj);
+		return formattedDate;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public long getTimeWorked() {
+		return timeWorked - System.currentTimeMillis();
+	}
+
+	public String getTitle() {
+		return title;
+	}
+}
