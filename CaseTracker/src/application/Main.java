056167b7ea955668927cdfa3e857bda9931f4f20
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2021-10-26 15:44:29 -0600

    textarea populates correctly

--- CaseTracker/src/application/Main.java
+++ CaseTracker/src/application/Main.java
@@ -31,62 +31,69 @@ public class Main extends Application {
 			// pane for creating the cases
 			VBox vbox = new VBox(15);
 			vbox.setPadding(new Insets(15, 15, 15, 15));
-			vbox.setStyle("-fx-border-color: black");
+//			vbox.setStyle("-fx-border-color: black");
 			TextField field = new TextField();
 			Button newcasebtn = new Button("New Case");
 			vbox.getChildren().addAll(new Label("New Case:"), field, newcasebtn);
 
-			// create combobox (dropdown) (doesnt work yet)
+			// create ListView
 			ListView<String> lview = new ListView<>();
-			lview.setPrefSize(20,110);
-//			cbo.getItems().addAll(items);
+			lview.setPrefSize(20, 110);
 
-			// displays the case titles
 			// register and handle 'new case' button
 			newcasebtn.setOnAction(e -> {
-				//make sure not to make duplicates
-				if(!titleList.contains(field.getText()))
+				// make sure not to make duplicates
+				if (!titleList.contains(field.getText()))
 					list.add(new Case(field.getText()));
 				System.out.println(list.toString());
 				for (Case element : list) {
-					// display each case title in the text node
-					if(!titleList.contains(element.getTitle()))
+					// add the case title to the titleList if it is not already in
+					if (!titleList.contains(element.getTitle()))
 						titleList.add(element.getTitle());
 				}
-				// adds the case titles to the listview 
+				// adds the case titles to the ListView
 				ObservableList<String> items = FXCollections.observableArrayList(titleList);
 				System.out.println(titleList.toString());
 				lview.getItems().clear();
 				lview.setItems(items);
 			});
-				
-			
-			// HBox for displaying all the cases in the list
+
 			// displays the case times
 			TextArea tarea = new TextArea();
 			tarea.setEditable(true);
 			tarea.setPrefColumnCount(5);
 			tarea.setPrefRowCount(8);
 			tarea.setWrapText(true);
-			tarea.setStyle("-fx-padding: 5px;" + "    -fx-border-insets: 5px;" + "    -fx-background-insets: 5px;");
+			tarea.setStyle("-fx-padding: 5px; -fx-border-insets: 5px;-fx-background-insets: 5px;");
 
-			// pane for the combo box
+			// pane for the ListView
 			StackPane paneforListView = new StackPane();
-			paneforListView.setPadding(new Insets(1,10,10,10));
+			paneforListView.setPadding(new Insets(1, 10, 10, 10));
 			paneforListView.getChildren().add(lview);
-
+			
+			// add listener to ListView
+			lview.getSelectionModel().selectedItemProperty().addListener(ov->{
+				int i = 0;
+				for(String s: titleList) {
+					if(s == lview.getSelectionModel().getSelectedItem()) {
+						i = titleList.indexOf(s);
+					}
+				}
+				tarea.setText(list.get(i).getTitle());
+			});
+			
 			BorderPane root = new BorderPane();
-			root.setStyle("-fx-border-color: red");
+//			root.setStyle("-fx-border-color: red");
 			root.setPadding(new Insets(15, 15, 15, 10));
 			root.setRight(vbox);
 			root.setCenter(tarea);
 			root.setTop(paneforListView);
-//			root.setRight(new CaseForm());
+			
 			Scene scene = new Scene(root, 500, 400);
-
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			primaryStage.setTitle("Case Tracker");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
