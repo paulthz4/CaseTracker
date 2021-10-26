318f070a1b4355abb18bb1116ea0046fb7f99e5f
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2021-10-25 22:17:44 -0600

    created general combo box to select cases

--- CaseTracker/src/application/Main.java
+++ CaseTracker/src/application/Main.java
@@ -3,14 +3,18 @@ package application;
 import java.util.ArrayList;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -29,11 +33,15 @@ public class Main extends Application {
 			TextField field = new TextField();
 			Button newcasebtn = new Button("New Case");
 			vbox.getChildren().addAll(new Label("New Case:"), field, newcasebtn);
-			
+
+			// create combobox (dropdown) (doesnt work yet)
+			ComboBox<Case> cbo = new ComboBox<>();
+//			cbo.getItems().addAll(items);
+
 			// displays the case titles
 			HBox hbox = new HBox(15);
 			hbox.setMaxWidth(100);
-			Text cases = new Text();
+			Text casesfield = new Text();
 			// register and handle 'new case' button
 			newcasebtn.setOnAction(e -> {
 				list.add(new Case(field.getText()));
@@ -41,25 +49,29 @@ public class Main extends Application {
 				String a = "";
 				for (Case element : list) {
 					a += element.getTitle() + " ";
-					cases.setText(a);
+					casesfield.setText(a);
+//					ObservableList<Case> items = FXCollections.observableArrayList(list);
+//					cbo.getItems().add(element.getTitle());
 //					System.out.print(element.getTitle());
 				}
+				ObservableList<Case> items = FXCollections.observableArrayList(list);
+				cbo.getItems().addAll(items);
 			});
-
+			hbox.getChildren().addAll(casesfield);
 //			cases.getText().bind(list);
 			// HBox for displaying all the cases in the list
-			
+
 			// displays the case times
 			TextArea tarea = new TextArea();
 			tarea.setEditable(true);
 			tarea.setPrefColumnCount(5);
 			tarea.setPrefRowCount(8);
 			tarea.setWrapText(true);
-			tarea.setStyle("-fx-padding: 5px;"
-					+ "    -fx-border-insets: 5px;"
-					+ "    -fx-background-insets: 5px;");
-			
-			hbox.getChildren().addAll(cases);
+			tarea.setStyle("-fx-padding: 5px;" + "    -fx-border-insets: 5px;" + "    -fx-background-insets: 5px;");
+
+			// pane for the combo box
+			StackPane paneforCbo = new StackPane();
+			paneforCbo.getChildren().add(cbo);
 
 			BorderPane root = new BorderPane();
 			root.setStyle("-fx-border-color: red");
@@ -67,6 +79,7 @@ public class Main extends Application {
 			root.setBottom(hbox);
 			root.setRight(vbox);
 			root.setCenter(tarea);
+			root.setTop(paneforCbo);
 //			root.setRight(new CaseForm());
 			Scene scene = new Scene(root, 500, 400);
 
@@ -78,20 +91,8 @@ public class Main extends Application {
 		}
 	}
 
-//	public void newCase() {
-//		list.add((field.getText()));
-//		System.out.println(list);
-//	}
-
 	public static void main(String[] args) {
 		launch(args);
 	}
 
-//	public VBox getConsol() {
-//		VBox vbox = new VBox(15);
-//		vbox.setStyle("-fx-background-color: #FFFFFF;");
-//		vbox.setStyle("-fx-border-color: white");
-//		vbox.getChildren().add(new Label("where times go"));
-//		return vbox;
-//	}
 }
--- CaseTracker/src/module-info.java
+++ CaseTracker/src/module-info.java
@@ -1,6 +1,7 @@
 module TrackCases {
 	requires javafx.controls;
 	requires javafx.graphics;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
