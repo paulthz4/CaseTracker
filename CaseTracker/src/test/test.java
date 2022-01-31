527dca80b5941153b4bcdb461a0fd67e501b05c7
paulthz4 <43190631+paulthz4@users.noreply.github.com>
2022-01-31 15:27:17 -0700

    moved into new folder

--- CaseTracker/src/test/test.java
+++ CaseTracker/src/test/test.java
@@ -1,5 +1,5 @@
-package application;
-
+package test;
+import application.Case;
 import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
--- CaseTracker/src/test/test2.java
+++ CaseTracker/src/test/test2.java
@@ -1,5 +1,5 @@
-package application;
-	
+package test;
+import application.Case;
 import java.util.ArrayList;
 import java.util.stream.IntStream;
 
